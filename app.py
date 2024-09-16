0ae5b21c0b69ab52ad274aecdae38a5d279ac61e
pendragonarthur <Arthur Quaresma>
2024-09-16 20:52:03 -0300

    fix: corrigi problemas de logica (ainda tem mais)

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import re
 import smtplib
 import imaplib
 import email
@@ -13,45 +14,59 @@ imap_server = os.getenv('IMAP_SERVER')
 smtp_server = os.getenv('SMTP_SERVER')
 
 # Lista de emails que devem ser automaticamente ignorados por qualquer motivo
-trashcan_mails = os.getenv('TRASHCAN_EMAILS').split(',')
+trashcan_mails = os.getenv('TRASHCAN_EMAILS')
+trashmails_list = trashcan_mails.split(',') 
+
+def regulate_email(text):
+    match = re.search(r'<(.+?)>', text)
+    if match:
+        return match.group(1)
+    return None
+
 
 def find_useless_emails():
+    # Email connection
     mail = imaplib.IMAP4_SSL(imap_server)
     mail.login(email_account, auth_password)
+    print(f'Login successfull! Current account: {email_account}')
     mail.select('inbox')
+
+    # Variables
+    senders_list = []
 
     status, messages = mail.search(None, 'ALL')
 
     if status != 'OK':
-        print('Failed to retrieve messages.')
+        print('Failed to access inbox.')
         return
     
     email_ids = messages[0].decode().split()
 
-    # Dividir em lote
-    batch_size = 5
-    total_size = len(email_ids)
+    for email_id in email_ids:
 
-    for i in range(0, 15, batch_size):
-        email_batch = email_ids[i:i + batch_size]
         status, data = mail.fetch(email_id, '(RFC822)')
-        raw_data = data[0][1]
-        filtered_data = email.message_from_bytes(raw_data)
-        sender = decode_header(filtered_data.get("from"))[0]
 
         if status != 'OK':
-            print(f'Error fetching email with ID{email_id}')
-            continue
+            print(f'Failed fetching email data. Email ID: {email_id}')
 
-        if sender not in trashcan_mails:
-            print('No trashcan senders found. Good!')
-        else: 
-            print(f'Deleting emails from: {sender}')
+        raw_data = data[0][1]
+        filtered_data = email.message_from_bytes(raw_data)
+        sender = decode_header(filtered_data.get("From"))[0][0]
+        decoded_sender = regulate_email(sender)
+        print(decoded_sender)
+
+        if sender in trashmails_list:
+            senders_list.append(sender) 
             mail.store(email_id, '+FLAGS', '\\Deleted')
 
-            
+            print(f'Deleting emails from: {senders_list}')
+                
+            option = input(f'Are you sure you want to procede? (Y/N)').lower()
+                
+            if option != 'y':
+                print('Canceling deletion.')
 
-    mail.expunge()
-    print(f'Processed batch {i // batch_size + 1} / {total_size // batch_size + 1}')
+    print("No trashcan emails found. Good!")
 
+           
 find_useless_emails()
