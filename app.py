cd6975b9ea27a6a1541830815504fb757c756606
pendragonarthur <devarthurquarxma@outlook.com>
2024-09-17 12:48:16 -0300

    feat: criei funçao especifica para conectar no email

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import email
 from email.header import decode_header
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 email_account = os.getenv('EMAIL_ACCOUNT')
@@ -15,21 +16,25 @@ smtp_server = os.getenv('SMTP_SERVER')
 
 # Lista de emails que devem ser automaticamente ignorados por qualquer motivo
 trashcan_mails = os.getenv('TRASHCAN_EMAILS')
-trashmails_list = trashcan_mails.split(',') 
+trashmails_list = trashcan_mails.split(',')
 
+
+# Função de regex para normalizar os dados de remetente dos email
 def regulate_email(text):
     match = re.search(r'<(.+?)>', text)
     if match:
         return match.group(1)
     return None
 
-
-def find_useless_emails():
-    # Email connection
+# Função para conectar o email
+def email_connection():
+    global mail
     mail = imaplib.IMAP4_SSL(imap_server)
     mail.login(email_account, auth_password)
     print(f'Login successfull! Current account: {email_account}')
     mail.select('inbox')
+
+def find_and_delete_useless_emails():
 
     # Variables
     senders_list = []
@@ -42,18 +47,21 @@ def find_useless_emails():
     
     email_ids = messages[0].decode().split()
 
-    for email_id in email_ids:
+    for email_id in reversed(email_ids):
 
         status, data = mail.fetch(email_id, '(RFC822)')
 
         if status != 'OK':
             print(f'Failed fetching email data. Email ID: {email_id}')
+            continue
 
         raw_data = data[0][1]
         filtered_data = email.message_from_bytes(raw_data)
         sender = decode_header(filtered_data.get("From"))[0][0]
         decoded_sender = regulate_email(sender)
-        print(decoded_sender)
+        # print(decoded_sender) 
+
+        #TODO: verificar a decodificação do sender, lembro que tinha uns que nao estavam corretamente normalizados
 
         if sender in trashmails_list:
             senders_list.append(sender) 
@@ -65,8 +73,11 @@ def find_useless_emails():
                 
             if option != 'y':
                 print('Canceling deletion.')
+                break
 
     print("No trashcan emails found. Good!")
 
-           
-find_useless_emails()
+
+email_connection()
+
+find_and_delete_useless_emails()
