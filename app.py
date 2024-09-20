bac281678adbe469da1fe3dee42cdc6224426323
pendragonarthur <Arthur Quaresma>
2024-09-19 23:04:32 -0300

    fix: arrumei fluxo e logica

--- app.py
+++ app.py
@@ -14,59 +14,66 @@ imap_server = os.getenv('IMAP_SERVER')
 smtp_server = os.getenv('SMTP_SERVER')
 
 # Lista de emails que devem ser automaticamente ignorados por qualquer motivo
-trashcan_mails = os.getenv('TRASHCAN_EMAILS')
-trashmails_list = trashcan_mails.split(',') 
+trashmails = os.getenv('TRASHCAN_EMAILS')
+regulated_trashmail_list = trashmails.split(',') 
 
+# Função de regex para normalizar os dados de remetente dos email
 def regulate_email(text):
+    if isinstance(text, bytes):
+        text = text.decode('utf-8', errors='ignore')
     match = re.search(r'<(.+?)>', text)
     if match:
         return match.group(1)
-    return None
+    return "ERROR DECODING EMAIL."
 
+# Função para conectar o email
+def email_connection():
+    mail = imaplib.IMAP4_SSL(imap_server)
+    login_account = mail.login(email_account, auth_password)
+    if login_account:
+        print(f'Login successfull! Current account: {email_account}')
+    else:
+        print(f'Failed to connect to email account: {email_account}')
+    mail.select('inbox')
+    return mail
 
 def find_useless_emails():
-    # Email connection
-    mail = imaplib.IMAP4_SSL(imap_server)
-    mail.login(email_account, auth_password)
-    print(f'Login successfull! Current account: {email_account}')
-    mail.select('inbox')
-
-    # Variables
-    senders_list = []
+    mail = email_connection()
 
     status, messages = mail.search(None, 'ALL')
-
-    if status != 'OK':
-        print('Failed to access inbox.')
-        return
-    
     email_ids = messages[0].decode().split()
+    total_emails = len(email_ids)
+    trashsenders_list = []
 
-    for email_id in email_ids:
+    print(f"Searching trashmails in {total_emails} emails. This may take a while.")
+
+    for email_id in reversed(email_ids):
 
         status, data = mail.fetch(email_id, '(RFC822)')
 
         if status != 'OK':
-            print(f'Failed fetching email data. Email ID: {email_id}')
+            print(f'Failed fetching email data from ID: {email_id}')
 
         raw_data = data[0][1]
         filtered_data = email.message_from_bytes(raw_data)
         sender = decode_header(filtered_data.get("From"))[0][0]
         decoded_sender = regulate_email(sender)
-        print(decoded_sender)
 
-        if sender in trashmails_list:
-            senders_list.append(sender) 
-            mail.store(email_id, '+FLAGS', '\\Deleted')
+        if decoded_sender in regulated_trashmail_list:
+            print(f'Sending {decoded_sender} to trash list. ')
+            trashsenders_list.append({'email_id': email_id, 'sender': decoded_sender})
+        
+    if not trashsenders_list:
+        print("No trashmail found. Good!")
+    
+    print(f"Emails from the following senders will be deleted: {trashsenders_list}. ")
 
-            print(f'Deleting emails from: {senders_list}')
-                
-            option = input(f'Are you sure you want to procede? (Y/N)').lower()
-                
-            if option != 'y':
-                print('Canceling deletion.')
+    option = input(f"\n Total of: {len(trashsenders_list)} emails are trash. \n Are you sure you want to procede? (Y/N)").lower()
+        
+    if option != 'y':
+        print('Canceling operation.')
+        return
+    
+    print(f'Trashmails deleted.')
 
-    print("No trashcan emails found. Good!")
-
-           
 find_useless_emails()
